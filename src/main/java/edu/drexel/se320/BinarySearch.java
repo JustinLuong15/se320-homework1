10b7cc3531a369096ca7156cfa63c25793e53027
JustinLuong15 <jl4494@drexel.edu>
2025-10-14 22:17:06 -0400

    feat: add binary search

--- src/main/java/edu/drexel/se320/BinarySearch.java
+++ src/main/java/edu/drexel/se320/BinarySearch.java
@@ -1,5 +1,7 @@
 package edu.drexel.se320;
 
+import java.util.NoSuchElementException;
+
 public class BinarySearch {
 
     // DO NOT MODIFY THIS SIGNATURE
@@ -8,7 +10,30 @@ public class BinarySearch {
     // code at runtime to test your test suite.
     protected static <T extends Comparable<T>> int binarySearchImplementation(T[] array, T elem) {
 	// TODO: Implement binary search here. The signature above sets you up to ensure the array elements have a compareTo method
-        if (elem == null) return -1;
-        return elem.compareTo(elem);
+        if (array == null)
+            throw new IllegalArgumentException("The array can't be null.");
+        if (array.length == 0)
+            throw new IllegalArgumentException("The array can't be empty.");
+        if (elem == null)
+            throw new IllegalArgumentException("elem can't be null.");
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            T midVal = array[mid];
+            int compareResult = midVal.compareTo(elem);
+
+            if (compareResult == 0) {
+                return mid;
+            } else if (compareResult < 0) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+
+        throw new NoSuchElementException("elem not found in the provided array.");
     }
 }
